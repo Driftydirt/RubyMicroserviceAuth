f61b79b650bbf0edc9797fb3ee4f00578ad11ccc
rt00492 <rt00492@surrey.ac.uk>
2022-04-25 17:15:22 +0100

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,32 @@
+class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      puts "lol"
+      puts resource.email
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
+
+  def record_not_unique(message)
+    render json: {
+      'errors': [
+        {
+          'status': '400',
+          'title': message
+        }
+      ]
+    }, status: 400
+  end
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
+end
